3fac5e3dd2b99faff642015f3f0df4ddae43dd79
Nicholas Alberts <Nick@Nicholass-MacBook-Pro-2.local>
2016-04-20 16:37:38 -0400

    hound

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ActiveRecord::Base
   has_one :profile, dependent: :destroy
 
-
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :trackable, :validatable
 
