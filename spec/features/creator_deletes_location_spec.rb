203436b827b285da15faa750a2e584d6e08db77d
Nicholas Alberts <Nick@Nicholass-MacBook-Pro-2.local>
2016-04-21 16:55:00 -0400

    basic edit

---
+++ spec/features/creator_deletes_location_spec.rb
@@ -0,0 +1,42 @@
+# require 'rails_helper'
+#
+# feature "creator deletes location" do
+#   let!(:user) { User.create(first_name: "Greg", last_name: "Ward", home_location: "Boston", email: "bob@la.com", password: "password1") }
+#
+#   scenario "creator deletes a city that he/she created" do
+#     visit 'users/sign_in'
+#     fill_in "Email", with: user.email
+#     fill_in "Password", with: user.password
+#     click_button "Log in"
+#
+#     visit "/locations"
+#
+#     click_link "Add New City"
+#     fill_in "Name", with: "New York"
+#     select "NY", from: "location_state"
+#     fill_in "Description", with: "The Big Apple"
+#     click_on "Add City"
+#     expect(page).to have_content("New York")
+#
+#     click_button "Delete City"
+#
+#     expect(page).to have_content("Successfully deleted city!")
+#     expect(page).to_not have_content("New York")
+#   end
+#
+#   scenario "unauthorized user does not see delete button" do
+#     FactoryGirl.create(:location)
+#
+#     visit 'users/sign_in'
+#     fill_in "Email", with: user.email
+#     fill_in "Password", with: user.password
+#     click_button "Log in"
+#     expect(page).to have_content "Signed in successfully."
+#
+#     visit "/locations"
+#
+#     click_link "Boston"
+#
+#     expect(page).to_not have_button("Delete City")
+#   end
+# end
--- spec/features/user_edits_presentation_spec.rb
+++ spec/features/user_edits_presentation_spec.rb
@@ -1,65 +1,54 @@
-# require 'rails_helper'
+require 'rails_helper'
+
+# As a presenter
+#   I want to edit my talk
+#   So that people see the correct information
 #
-# feature "creator edits location data and user cannot edit" do
-#   let!(:user) { User.create(first_name: "Britta", last_name: "Carroll", home_location: "Boston", email: "brittacarroll@la.com", password: "password1") }
-#   let!(:admin) { User.create(first_name: "Greg", last_name: "Ward", home_location: "Boston", email: "greg@la.com", password: "password1", role: "admin") }
-#
-#   scenario "creator of location edits location" do
-#     visit 'users/sign_in'
-#     fill_in "Email", with: user.email
-#     fill_in "Password", with: user.password
-#     click_button "Log in"
-#
-#     visit "/locations"
-#
-#     click_link "Add New City"
-#     fill_in "Name", with: "New York"
-#     select "NY", from: "location_state"
-#     fill_in "Description", with: "The Big Apple"
-#     click_on "Add City"
-#
-#     click_link "Edit"
-#
-#     fill_in 'Name', with: "Miami"
-#     select 'FL', from: "location_state"
-#     fill_in 'Description', with: "Hot"
-#
-#     click_button "Submit"
-#
-#     expect(page).to have_content "Successfully updated city!"
-#     expect(page).to have_content "Miami"
-#     expect(page).to have_content "Hot"
-#   end
-#
-#   scenario "user (not creator) tries to edit location" do
-#     boston = FactoryGirl.create(:location)
-#
-#     visit 'users/sign_in'
-#     fill_in "Email", with: user.email
-#     fill_in "Password", with: user.password
-#     click_button "Log in"
-#
-#     visit "/locations"
-#
-#     click_link "Boston"
-#
-#     expect(page).to_not have_content "Edit"
-#     expect(page).to have_content boston.description
-#   end
-#
-#   scenario "admin tries to edit location" do
-#     boston = FactoryGirl.create(:location)
-#
-#     visit 'users/sign_in'
-#     fill_in "Email", with: admin.email
-#     fill_in "Password", with: admin.password
-#     click_button "Log in"
-#
-#     visit "/locations"
-#
-#     click_link "Boston"
-#
-#     expect(page).to_not have_content "Edit"
-#     expect(page).to have_content boston.description
-#   end
-# end
+#   Acceptance Criteria -
+#   [ ] On the talk edit page, I should see fields for title, slides and description.
+#   [ ] Clicking on the "upload slides" button should bring up a prompt for uploads.
+#   [ ] If I did not fill out the information correctly, I will be presented with an error message.
+#   [ ] If I save my changes, I should be brought back to my talk show page.
+#   [ ] Admins and the creator's of the lighting talk may delete the lightning talk if they wish. Only the user can edit the lighting talk.
+
+feature "creator edits presentation data" do
+  let(:user) do
+    FactoryGirl.create(:user)
+  end
+  let(:user2) do
+    FactoryGirl.create(:user)
+  end
+
+  let!(:presentation) { FactoryGirl.create(:presentation, user: user) }
+
+  scenario "creator of presentation edits presentation" do
+    visit 'users/sign_in'
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+    click_button "Log in"
+
+    click_link presentation.title
+    
+    click_link "Edit Presentation"
+
+    fill_in 'Title', with: "What Even Is CSS?"
+    fill_in 'Description', with: "AAAAAHHHHHHHH"
+
+    click_button "Submit"
+
+    expect(page).to have_content "What Even Is CSS?"
+    expect(page).to have_content "AAAAAHHHHHHHH"
+  end
+
+  scenario "not-creator tries to edit presentation" do
+
+    visit 'users/sign_in'
+    fill_in "Email", with: user2.email
+    fill_in "Password", with: user2.password
+    click_button "Log in"
+
+    visit presentations_path(presentation)
+
+    expect(page).to_not have_content "Edit"
+  end
+end
