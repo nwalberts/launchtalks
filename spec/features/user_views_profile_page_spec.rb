ce32bd5b83e73edc7733cfc939bdfd38411753be
Nicholas Alberts <Nick@Nicholass-MacBook-Pro-2.local>
2016-04-21 11:10:23 -0400

    hound2

--- spec/features/user_views_profile_page_spec.rb
+++ spec/features/user_views_profile_page_spec.rb
@@ -1,13 +1,4 @@
 require 'rails_helper'
-# As a an authenticated user
-# I want to see my profile page
-# So that I can see my profile details and a list of my past talks
-
-# Acceptance Criteria -
-# [ ] On the profile show page, I should see my name, email address, bio and profile avatar if I have one
-# [ ] I should see a link to edit my name, email bio and profile picture.
-# [ ] I should see each of my past lightning talks, and each should be a link to the details of the talk.
-# [ ] If I am not signed in, I will not be allowed to see anyones profile page, and be prompted to sign in.
 
 feature "user may view their profile information" do
   let(:user) do
