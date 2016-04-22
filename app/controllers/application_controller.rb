ed5b3f9769d9f8f476c8bed03f0a8c56d66856e4
Nicholas Alberts <Nick@Nicholass-MacBook-Pro-2.local>
2016-04-22 13:37:51 -0400

    houndsass

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-
-  def after_sign_in_path_for(user)
-    root_path
-  end
-
+  
   def after_sign_out_path_for(type)
     if type == :user
       root_path
