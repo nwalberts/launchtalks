52b2f0e9c36c94c3a783c51b7ec67521bbd413c8
Nicholas Alberts <Nick@Nicholass-MacBook-Pro-2.local>
2016-04-26 09:15:53 -0400

    meetup testing initial

--- spec/features/user_views_meetups_index_spec.rb
+++ spec/features/user_views_meetups_index_spec.rb
@@ -1,1 +1,19 @@
 
+require 'rails_helper'
+
+feature "visitor sees list of meetups" do
+  let!(:user) { FactoryGirl.create(:user) }
+  scenario "visits meetup index and sees list of meetups" do
+    visit 'users/sign_in'
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+    click_button "Log in"
+    visit meetups_path
+
+    expect(page).to have_selector('li')
+    expect(page).to have_content "Meetups in Boston"
+    expect(page).to have_link "View Profile"
+    expect(page).to have_link "LaunchTalks"
+    expect(page).to have_link "Log Out"
+  end
+end
